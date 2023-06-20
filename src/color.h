204b143529aba94a6516d02e1db2e3feb2f96bb7
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-06-20 20:09:09 +0800

    chapter 3

---
+++ src/color.h
@@ -0,0 +1,22 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+class Color : private Vec3 {
+  public:
+    double& r = x;
+    double& g = y;
+    double& b = z;
+
+  Color(double r, double g, double b) : Vec3(r, g, b) {}
+};
+
+inline void write_color(std::ostream& out, Color pixel_color) {
+  out << static_cast<int>(255.999 * pixel_color.r) << ' '
+      << static_cast<int>(255.999 * pixel_color.g) << ' '
+      << static_cast<int>(255.999 * pixel_color.b) << ' ';
+}
+
+#endif
---
+++ src/point3.h
@@ -0,0 +1,8 @@
+#ifndef POINT3_H
+#define POINT3_H
+
+#include "vec3.h"
+
+using Point3 = Vec3;
+
+#endif
